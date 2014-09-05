17fb69d1c30f7db13940778bc0f831bf7e10f147
Kirby Powell <powell.ki@husky.neu.edu>
2014-09-05 15:10:15 -0400

    completed exercises 1 & 2 and updated notebook

---
+++ activity1.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname activity1) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+;exercise #1
+31622400
+(* 2 15811200)
+(* 2 (/ 31622400 2))
+
+;exercise #2
+(cond
+  [(> (/ 100 3) (/ (+ 100 3) (+ 3 3))) true]
+  [else false])
+
+;exercise #3
